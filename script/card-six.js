1c108c2976b59fe2a5636d569736017147430a06
badhantinku <badhandastinku4774@gmail.com>
2025-08-05 02:00:24 +0600

    Added my counting is done

---
+++ script/card-six.js
@@ -0,0 +1,14 @@
+document.getElementById('card-six').addEventListener('click', function (event) {
+    event.preventDefault();
+    const plusPoint = document.getElementById('plusPoint').innerText;
+    const convertedPlusPoit = parseFloat(plusPoint)
+    const sum = convertedPlusPoit + 1;
+    document.getElementById('plusPoint').innerText = sum;
+})
+document.getElementById('card-six').addEventListener('click', function (event) {
+    event.preventDefault();
+    const spend = document.getElementById('spend').innerText;
+    const convertedSpend = parseFloat(spend)
+    const sum = convertedSpend - 1;
+    document.getElementById('spend').innerText = sum;
+})
